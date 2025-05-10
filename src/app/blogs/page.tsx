4996995b9767ca43cbbee30cb03ef39d49798919
lcdkhoa <lcdkhoa@hotmail.com>
2025-05-10 20:41:11 +0700

    chore: temporarily hide main blog page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,98 +1,21 @@
 'use client';
 
-import BlogCard from '@/components/Cards/BlogCard';
-import Loading from '@/components/Loading';
-import { Box, Container, Grid, Tab, Tabs, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Grid, Typography } from '@mui/material';
 
-import Pagination from './components/Pagination';
-import { BlogCardList } from './configs/constants';
-
-const POSTS_PER_PAGE = 6;
-
-export default function BlogList({ blogParam }: { blogParam?: string }) {
-	const [currentPage, setCurrentPage] = useState(1);
-	const [isLoading, setIsLoading] = useState(true);
-	const [tab, setTab] = useState(0);
-	const [favorites, setFavorites] = useState<string[]>([]);
-
-	const updateFavorites = () => {
-		if (typeof window !== 'undefined') {
-			const fav = localStorage.getItem('favoriteBlogs');
-			if (fav) setFavorites(JSON.parse(fav));
-		}
-	};
-
-	const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
-		setTab(newValue);
-	};
-
-	useEffect(() => {
-		setIsLoading(false);
-	}, [blogParam]);
-
-	const totalPages = Math.ceil(BlogCardList.length / POSTS_PER_PAGE);
-	const currentPostPerPage = BlogCardList.slice(
-		(currentPage - 1) * POSTS_PER_PAGE,
-		currentPage * POSTS_PER_PAGE
-	);
-
-	useEffect(() => {
-		updateFavorites();
-		window.addEventListener('storage', updateFavorites);
-		return () => {
-			window.removeEventListener('storage', updateFavorites);
-		};
-	}, []);
-
+export default function Blog() {
 	return (
-		<Box sx={{ minHeight: 'calc(100vh - 64px - 50px)', display: 'flex', flexDirection: 'column' }}>
-			<Container maxWidth="lg" sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-				<Loading isLoading={isLoading} />
-				<Grid container justifyContent="center" mt={5}>
-					<Tabs value={tab} onChange={handleTabChange}>
-						<Tab
-							label={
-								<Typography variant="subtitle1" color="text.primary">
-									All
-								</Typography>
-							}
-						/>
-						<Tab
-							label={
-								<Typography variant="subtitle1" color="text.primary">
-									Favorites
-								</Typography>
-							}
-						/>
-					</Tabs>
-				</Grid>
-				<Grid
-					container
-					alignContent={'center'}
-					justifyContent={'center'}
-					sx={{ overflow: 'hidden', display: 'flex', flexDirection: 'row' }}
-					mt={2}
-				>
-					{(tab === 0
-						? currentPostPerPage
-						: BlogCardList.filter((blog) => favorites.includes(blog.slug))
-					).map((blog) => {
-						return (
-							<Grid key={blog.id}>
-								<BlogCard {...blog} />
-							</Grid>
-						);
-					})}
-				</Grid>
-			</Container>
-			<Box sx={{ display: 'flex', justifyContent: 'center', mt: 'auto', mb: 1 }}>
-				<Pagination
-					count={totalPages}
-					page={currentPage}
-					onChange={(page) => setCurrentPage(page)}
-				/>
-			</Box>
-		</Box>
+		<Grid
+			justifyContent={'center'}
+			alignContent={'center'}
+			sx={{ display: 'flex', flexDirection: 'column' }}
+			mt={10}
+		>
+			<Typography variant="h1" color="text.primary" textAlign={'center'} gutterBottom>
+				Building Blog Section
+			</Typography>
+			<Typography variant="h5" color="text.secondary" textAlign={'center'}>
+				Coming soon
+			</Typography>
+		</Grid>
 	);
 }
